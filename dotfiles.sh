a4169be5a970a27546c601654b09ae480f486b6b
John Shanahan <shanahan.jrs@gmail.com>
2014-09-18 13:34:51 -0400

    Added dotfiles.sh and install script

---
+++ dotfiles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+#dotfiles.sh
+# John Shanahan
+# Makes and retrieves your dotfiles from your github account.
+
+# Must be run with 'sudo'
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+# dotfiles.sh install script
+
+# 1. mv dotfiles.sh to your path
+# 2. mv ~/dotfiles/ to ~/.dotfiles or some other
