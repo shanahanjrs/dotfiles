37b1fd3105cd494c87b6fb834db2d5948650f5a5
John Shanahan <shanahan.jrs@gmail.com>
2014-09-21 18:41:38 -0400

    Added TODO, added sudo check

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,17 @@
-#!/usr/bin/sh
+#!/bin/bash
 
+##############################################################
 #dotfiles.sh
 # John Shanahan
 # Makes and retrieves your dotfiles from your github account.
 
+# TODO
+# Add option to delete everything on logout using .bash_logout
+
+
 # Must be run with 'sudo'
+if [ "$(id -u)" != "0" ]; then
+    echo "Please run script with '$ sudo dotfiles'"
+    exit 1
+fi
+
